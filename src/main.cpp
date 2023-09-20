7d76dd06efd205ba2a6e61b9123ac9f42329590c
Magnus <mp-se@users.noreply.github.com>
2023-09-20 07:46:17 +0200

    Updated example for littlefs

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,38 @@
 #include <Arduino.h>
 #include <FS.h>
 
-/*#include <SPIFFS.h>
+//#define USE_SPIFFS true
 
-void setup() {
-  Serial.begin(115200L);
-  Serial.printf("Starting\n");
-
-  SPIFFS.begin(true);
-
-  File f = SPIFFS.open("/test", "w");
-
-  if(f) {
-    f.write('C');
-    f.close();
-    Serial.printf("Write successful\n");
-  }else {
-    Serial.printf("Failed to open file\n");
-  }
-}*/
-
-
+#ifdef USE_SPIFFS
+#include <SPIFFS.h>
+#define LittleFS SPIFFS
+#else
 #include <LittleFS.h>
+#endif
+
 void setup() {
   Serial.begin(115200L);
   Serial.printf("Starting\n");
 
   LittleFS.begin(true);
- 
-  File f = LittleFS.open("/test", "w");
 
+  File f = LittleFS.open("/myfile.txt", "r");
+  if(f) {
+    String s = f.readStringUntil('\r');
+    Serial.println(s.c_str());
+    f.close();
+    Serial.printf("Read successful\n");
+  }else {
+    Serial.printf("Failed to open file for reading\n");
+  }
+
+  f = LittleFS.open("/test", "w");
   if(f) {
     f.write('C');
     f.close();
     Serial.printf("Write successful\n");
   }else {
-    Serial.printf("Failed to open file\n");
+    Serial.printf("Failed to open file for writing\n");
   }
 }
 
